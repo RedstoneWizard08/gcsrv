55bc76368cb139aef06090bfd1d39760fe46b197
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-12-31 15:33:47 -0800

    Revert 0.4.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-VERSION=0.4.0+1.12.2_14.23.5.2859
+VERSION=0.3.1+1.12.2_14.23.5.2859
 
 [[ ! -d build ]] && mkdir build
 
-packwiz cf export --side client --output build/client/gcsrv-pack-v$VERSION-client.zip
-packwiz cf export --side server --output build/server/gcsrv-pack-v$VERSION-server.zip
+packwiz cf export --side client --output build/gcsrv-pack-v$VERSION-client.zip
+packwiz cf export --side server --output build/gcsrv-pack-v$VERSION-server.zip
 
-zip -r build/gcsrv-pack-v$VERSION.zip \
-    build/client/gcsrv-pack-v$VERSION-client.zip \
-    build/server/gcsrv-pack-v$VERSION-server.zip
+zip -r gcsrv-pack-v$VERSION.zip build
